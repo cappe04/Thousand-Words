cff04230d67f81cc496c11844e0aa5793c0a3bd8
Casper Bené <48201144+cappe04@users.noreply.github.com>
2024-03-21 01:58:41 +0100

    lang_api route

--- server/app/api.py
+++ server/app/api.py
@@ -1,8 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 bp = Blueprint("api", __name__)
 
-@bp.route("/")
-def main():
-    return "Hello, World!"
+@bp.route("/lang_api/<lang>")
+def lang_api(lang):
+    # Get table
+    if (table := request.args.get("table")) is None:
+        return "Record not found", 400
+    
+    # Slicing
+    start = request.args.get("start", 0)
+    end = request.args.get("end", -1)
+
+    return {
+        "lang": {
+            "name": lang,
+            "table": table,
+            "span": {
+                "start": start,
+                "end": end
+            }
+        }
+    }
 
