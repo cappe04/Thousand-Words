c42fe29841e0b50ba1bbe2625b8484ce6564552e
Casper Bené <48201144+cappe04@users.noreply.github.com>
2024-03-29 00:52:18 +0100

    old index -> main

--- client/app/index.js
+++ client/app/index.js
@@ -1,152 +1,50 @@
-import { Stack, useRouter } from "expo-router";
-import { View, Text, SafeAreaView, ActivityIndicator } from "react-native"
+import { Redirect, Stack } from "expo-router";
+import { loadUserdata } from "../src/data";
+import { useEffect, useState } from "react";
+import { SafeAreaView, ActivityIndicator, View, Text } from "react-native";
+import { COLORS } from "../constants";
 
-import { HeaderIconButton, NavBar, LearnWords, PracticeWords, WordList, FlashcardOptions } from "../components"
+import api from "../src/api";
 
-import { COLORS, SHADOWS } from "../constants";
-import icons from "../constants/icons";
-import { React, useEffect, useState } from "react";
-
-async function getTestWords(table, day){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const data = [
-                {
-                    day: day,
-                    words: [
-                        { word: "принести", translation: "to bring", type: "verb", },
-                        { word: "комната", translation: "a room", type: "noun", },
-                        { word: "часть", translation: "part, share, department", type: "noun", },
-                        { word: "сегодня", translation: "today", type: "adverb", },
-                    ]
-                },
-                {
-                    day: 4,
-                    words: [
-                        { word: "бог", translation: "god", type: "noun" },
-                        { word: "вместе", translation: "together", type: "adverb" },
-                        { word: "взгляд", translation: "look, glance; view", type: "noun" },
-                        { word: "ходить", translation: "to go, walk", type: "verb" },
-                        { word: "зачем", translation: "what for, why", type: "adverb" },
-                    ]
-                },
-                {
-                    day: 2,
-                    words: [
-                        { word: "первый", translation: "first, front, former", type: "adjective, number" },
-                        { word: "день", translation: "day", type: "noun" },
-                        { word: "тут", translation: "here, now, then", type: "adverb" },
-                        { word: "во", translation: "in, at; super, exactly", type: "preposition, particle" },
-                        { word: "ничто", translation: "nothing", type: "pronoun" },
-                        { word: "потом", translation: "afterwards, then", type: "adverb" },
-                        { word: "очень", translation: "very", type: "adverb" },
-                    ]
-                },
-            ];
-            resolve(data);
-        }, 2000);
-    })
-}
-
-async function fetchWords(lang, table, day){
-    const url = `http://192.168.1.51:5000/lang_api/${lang}?table=${table}`;
-    const fetchedData = [];
-
-    if(day == null){
-        const data =  await fetch(url).then(response => response.json());
-        fetchedData.push({ day: 0, words: data.map((word) => {
-            return { word: word[1], translation: word[2], type: word[3] } 
-        })})
-    }
+const OnLoad = () => {
     
-    const layout = [1, 2, 3, 5, 8, 13, 21];
-    const daySize = 20;
-
-    const promises = layout.map(async (i) => {
-        const currentDay = day-i+1
-        if(currentDay > 0){
-            const data = await fetch(url + `&start=${(currentDay-1)*daySize+1}&end=${currentDay*daySize}`).then(response => response.json())
-            fetchedData.push({ day: currentDay, words: data.map((word) => {
-                return { word: word[1], translation: word[2], type: word[3] }
-            })});
-        }
-    });
-
-    await Promise.all(promises);
-    return fetchedData;
-}
-
-const Home = () => {
-    const router = useRouter();
+    const [loading, setLoading] = useState(true);
     
-    const [currentDay, setCurrentDay] = useState(1); // load from file
-    const [tab, setTab] = useState(0);
-    const [showOptions, setShowOptions] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const [words, setWords] = useState([]);
-
-    const currentLang = "ru"; // load from file or from menu
-    const currentTable = "common"; // load from file or from menu
-
-    const getData = async () => {
-        setIsLoading(true)
+    const loadData = async () => {
         try {
-            const response = await fetchWords(currentLang, currentTable, currentTable == "common" ? currentDay: null);
-            setWords(response);
+            await api.loadMetadata();
+            await loadUserdata();
         } catch (error) {
-            console.error(error)
+            console.log(error);
         } finally {
-            setIsLoading(false);
+            setLoading(false);
         }
     }
 
     useEffect(() => {
-        getData();
-        setTab(0);
-    }, [currentDay])
+        loadData();
+    }, []);
 
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-            <Stack.Screen 
-                options={{
-                    headerStyle: { backgroundColor: COLORS.lightWhite },
-                    headerLeft: () => (
-                        <HeaderIconButton icon={icons.MENU} dimension={"60%"} callback={() => {}}/>
-                    ),
-                    headerRight: () => (
-                        <HeaderIconButton icon={icons.OPTIONS} dimension={"60%"} callback={() => {setShowOptions(!showOptions)}}/>
-                    ),
-                    headerTitle: currentTable != "common" ? currentTable: `Day ${currentDay}`,
-                }}
-            />
-        
-            {isLoading ? (
+        <SafeAreaView style={{ backgroundColor: COLORS.lightWhite, flex: 1 }}>
+            <Stack.Screen options={{headerStyle: { backgroundColor: COLORS.lightWhite }, headerTitle: ""}} />
+            { loading ? (
                 <View style={{
-                    marginTop: "30%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    flex: 0.9,
                 }}>
                     <ActivityIndicator />
+                    <Text style={{
+                        padding: 10,
+                    }}>Loading...</Text>
                 </View>
             ) : (
-                <View>
-                    <View style={{ paddingTop: 17 }}>
-                        <NavBar 
-                            tabs={["Learn Words", "Practice Words", "Word List"]} 
-                            callback={(tab) => { setTab(tab) }} 
-                            skip={currentTable != "common" ? 1: undefined}
-                        />
-                    </View>
-
-                    {tab === 0 && <LearnWords words={words[0]}/>}
-                    {tab === 1 && <PracticeWords words={words.slice(1)}/>}
-                    {tab === 2 && <WordList words={words} divided={currentTable == "common"}/>}
-                        
-                </View>
+                <Redirect href={"/menu"}/>
             )}
-            
-            <FlashcardOptions show={showOptions && currentTable == "common"} currentDay={currentDay} setCurrentDay={setCurrentDay}/>
-        
         </SafeAreaView>
     )
 }
 
-export default Home;
+export default OnLoad;
---
+++ client/app/main.js
@@ -0,0 +1,93 @@
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { View, Text, SafeAreaView, ActivityIndicator } from "react-native"
+
+import { HeaderIconButton, NavBar, LearnWords, PracticeWords, WordList, FlashcardOptions } from "../components"
+
+import { COLORS, SHADOWS } from "../constants";
+import icons from "../constants/icons";
+import { React, useEffect, useState } from "react";
+
+import state from "../src/state";
+import api from "../src/api";
+
+
+const Main = () => {
+    const router = useRouter();
+
+    const { id, table, lang } = useLocalSearchParams();
+    const metadata = state.metadata[lang][table];
+    const userdata = state.userdata[lang][table];
+
+    const [currentId, setCurrentId] = useState(parseInt(id)); // load from file
+    const [tab, setTab] = useState(0);
+    const [showOptions, setShowOptions] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [words, setWords] = useState([]);
+
+    const getData = async () => {
+        setIsLoading(true)
+        try {
+            const response = metadata.formatting.complex 
+                ? await api.fetchBaches(lang, table, currentId)
+                : await api.fetchWords(lang, table);
+            setWords(response);
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+        setTab(0);
+        if(metadata.formatting.complex)
+            userdata.currentId = currentId;
+    }, [currentId])
+
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen 
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerLeft: () => (
+                        <HeaderIconButton icon={icons.MENU} dimension={"60%"} callback={router.back}/>
+                    ),
+                    headerRight: () => (
+                        <HeaderIconButton icon={icons.OPTIONS} dimension={"60%"} callback={() => {setShowOptions(!showOptions)}}/>
+                    ),
+                    headerTitle: metadata.formatting.complex ? (metadata.formatting.batch_title.title + (metadata.formatting.batch_title.index ? ` ${currentId}`: "")): metadata.title,
+                }}
+            />
+        
+            {isLoading ? (
+                <View style={{
+                    marginTop: "30%",
+                }}>
+                    <ActivityIndicator />
+                </View>
+            ) : (
+                <View>
+                    <View style={{ paddingTop: 17 }}>
+                        <NavBar 
+                            tabs={["Learn Words", "Practice Words", "Word List"]} 
+                            callback={(tab) => { setTab(tab) }} 
+                            skip={!metadata.formatting.complex ? 1: undefined}
+                        />
+                    </View>
+
+                    {tab === 0 && <LearnWords words={metadata.formatting.complex ? words[0].words: words}/>}
+                    {tab === 1 && <PracticeWords words={words.slice(1)}/>}
+                    {tab === 2 && <WordList words_or_batch={words} is_batch={metadata.formatting.complex} batch_title={metadata.formatting.batch_title?.title}/>}
+                        
+                </View>
+            )}
+            
+            <FlashcardOptions show={showOptions && metadata.formatting.complex} currentDay={currentId} setCurrentDay={setCurrentId}/>
+        
+        </SafeAreaView>
+    )
+}
+
+export default Main;
