4307147fd013a87b644e8e7c3c1d1ccf33591393
Casper Bené <48201144+cappe04@users.noreply.github.com>
2024-04-13 23:36:07 +0200

    MORE ICONS!!"!!!!!!!!!!

--- client/ios-v2.0.0/constants/icons.js
+++ client/ios-v2.0.0/constants/icons.js
@@ -1,9 +1,15 @@
 import error from "../assets/icons/error.png"
 import dropdown from "../assets/icons/dropdown.png"
 import goto_arrow from "../assets/icons/goto_arrow.png"
+import menu from "../assets/icons/menu.png"
+import settings from "../assets/icons/settings.png"
+import arrow_left from "../assets/icons/arrow_left.png"
 
 export default {
     error,
     dropdown,
     goto_arrow,
+    menu,
+    settings,
+    arrow_left,
 }
