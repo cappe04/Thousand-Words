aa55bfb00b38b1de92c2275a03805bb56760c508
Casper Bené <48201144+cappe04@users.noreply.github.com>
2024-04-22 14:29:35 +0200

    docker go brrrrr

---
+++ run.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+if "%1"=="" (
+    echo No argument provided.
+    goto end:
+)
+
+if %1=="server" (
+    echo Running server at 192.168.1.51:5000.
+    docker run -p 192.168.1.51:5000:8080 thousand-words
+) else (
+    echo Not implemented or invalid.
+)
+
+:end
+
+
--- server/run.py
+++ server/run.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 from os import environ
 
 if __name__ == "__main__":
-    load_dotenv("../.env")
-    host, port = environ.get("HOST"), environ.get("PORT")
+    load_dotenv()
+    host, port = environ.get("HOST_INTERNAL"), environ.get("PORT_INTERNAL")
     create_app().run(
         host=host,
         port=port,
